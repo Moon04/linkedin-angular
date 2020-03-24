b89351a9591497617eb639f19fba569e6aba91a4
Maryam Talal <40206568+MaryamTSelim@users.noreply.github.com>
2020-03-25 00:10:27 +0200

    Update and rename header.component.ts to homePageheader.component.ts

---
+++ src/app/header/homePageheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homePageheader',
+  templateUrl: './homePageheader.component.html',
+  styleUrls: ['./homePageheader.component.scss']
+})
+export class HomePageHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
