7cd2ae60a13dfc64ae4e9b037494ee93428df18f
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-03-24 21:23:50 +0200

    Landing Page / Login / Sign Up Components

--- linkedin/src/app/app-routing.module.ts
+++ linkedin/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingPageComponent } from "./features/landing-page/landing-page.component";
+import { LoginComponent } from "./core/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  // {path: 'register', component: regform},
+
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
